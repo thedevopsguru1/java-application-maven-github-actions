cd33be9f80ce2c7ef8930ef0c593192066713d97
devopstrainingschool <107158398+devopstrainingschool@users.noreply.github.com>
2023-03-07 08:09:47 -0600

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Welcome to DevOps training school. This is the Java  Maven Project1. If you see this , then all good! ";
+		return "Greetings from Spring Boot!, Welcome to DevOps training school. This is the Java Maven webhook ";
 	}
 
 }
